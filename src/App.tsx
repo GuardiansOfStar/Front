9de4951f7cb1d8c21d07c6f73b1f6388e8f36de3
sandy <dianwls032604@naver.com>
2025-04-27 21:34:16 +0900

    Stashed changes 적용

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// src/App.tsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import ScenarioSelectPage from './components/ScenarioSelectPage';
+import MapPage from './components/MapPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/scenarios" element={<ScenarioSelectPage />} />
+        <Route path="/map" element={<MapPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
