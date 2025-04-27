8d8855b622abc791b4f470b8b292923fe80b1042
sandy <dianwls032604@naver.com>
2025-04-28 01:03:11 +0900

    setting button

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -5,7 +5,9 @@ const Setting = () => {
             alt="setting"
             className="absolute top-5 right-10 
             w-[100px] h-auto
-            z-50"
+            cursor-pointer
+            z-50
+            active:scale-90 transition-transform duration-150"
         />
     );
 };
