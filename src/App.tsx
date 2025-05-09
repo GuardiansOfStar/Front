550d51fec3a63deed57b3bebce57bd463cd72caf
sandy <dianwls032604@naver.com>
2025-05-09 17:31:36 +0900

    Save changes before switching to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AspectRatioContainer from './components/layout/AspectRatioContainer';
 import HomePage from './pages/home/HomePage';
 import ScenarioSelectPage from './pages/scenarioSelect/ScenarioSelectPage';
 import MapPage from './pages/map/MapPage';
+<<<<<<< HEAD
 import EduScreen from './pages/ResultScreen/EduScreen';
 import Certificate from './pages/ResultScreen/Certificate';
 import PersonalInfo from './pages/ResultScreen/PersonalInfo';
 import StarSurvey from './pages/ResultScreen/StarSurvey';
+=======
+import ProloguePage from './pages/prologue/ProloguePage';
+import DrivingPrepPage from './pages/driving/DrivingPrepPage';
+import QuestPage from './pages/quest/QuestPage';
+>>>>>>> main
 
 function App() {
   return (
     <Router>
+<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/result" element={<EduScreen />} />
@@ -20,6 +28,18 @@ function App() {
         <Route path='/info' element={<PersonalInfo />} />
         <Route path='/star' element={<StarSurvey />} />
       </Routes>
+=======
+      <AspectRatioContainer ratio="5/4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/scenarios" element={<ScenarioSelectPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/prologue" element={<ProloguePage />} />
+          <Route path="/driving-prep" element={<DrivingPrepPage />} />
+          <Route path="/quest" element={<QuestPage />} />
+        </Routes>
+      </AspectRatioContainer>
+>>>>>>> main
     </Router>
   );
 }
