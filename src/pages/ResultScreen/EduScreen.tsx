4dd0218b5ce6fb81e66c7f3561513911681e4406
sandy <dianwls032604@naver.com>
2025-05-19 20:32:35 +0900

    에듀화면 배경 수정

--- src/pages/ResultScreen/EduScreen.tsx
+++ src/pages/ResultScreen/EduScreen.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Background from '../../components/ui/Background';
+import CompleteBackground from '../../components/ui/CompleteBackground';
 
 const EduScreen = () => {
   const navigate = useNavigate();
-  
+  {/*
   useEffect(() => {
     console.log("EduScreen - 결과 화면 표시 중");
     // 5초 후 자동으로 수료증 화면으로 이동
@@ -15,25 +15,25 @@ const EduScreen = () => {
     
     return () => clearTimeout(timer);
   }, [navigate]);
-
+*/}
   return (
     <div
-      className="relative w-full h-screen overflow-hidden flex flex-col items-center justify-center"
+      className="relative w-full h-full overflow-hidden flex flex-col items-center justify-center"
       onClick={() => navigate('/certificate')}
     >
-      <Background />
+      <CompleteBackground />
 
       <img
-        src="/assets/images/score_background.png"
+        src="/assets/images/score.png"
         alt="점수 이미지"
-        className="w-[60%] z-50 mb-5"
+        className="w-[65%] z-50 mb-5"
       />
 
-      <div className="flex w-[75%] justify-between z-50">
-        <div className="w-[48%] h-40 bg-white border-8 border-green-600 rounded-lg flex items-center justify-center text-green-600 font-bold text-xl">
+      <div className="flex w-[85%] h-[50%] justify-between z-50">
+        <div className="w-[48%] h-1/1 bg-white border-8 border-green-600 rounded-lg flex items-center justify-center text-green-600 font-bold text-xl">
           칭찬해요
         </div>
-        <div className="w-[48%] h-40 bg-white border-8 border-green-600 rounded-lg flex items-center justify-center text-green-600 font-bold text-xl">
+        <div className="w-[48%] h-1/1 bg-white border-8 border-green-600 rounded-lg flex items-center justify-center text-green-600 font-bold text-xl">
           기억해요
         </div>
       </div>
